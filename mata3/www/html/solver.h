8840a12f88b56823154a4f042d4dfb094ff9ac61
Lukasz Rzodeczko <rzodeczkol@gmail.com>
2018-12-31 12:07:08 +0100

    klasa solver

---
+++ mata3/www/html/solver.h
@@ -0,0 +1,30 @@
+#ifndef SOLVER_H_
+#define SOLVER_H_
+
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <regex>
+
+using namespace std;
+
+class solver {
+public:
+	vector<vector<float>> LTE_throughput_per_user;
+	vector<vector<float>> WIFI_throughput_per_user;
+	float optimal_solution;
+	bool is_problem_feasible;
+	bool unexpected_error;
+	solver();
+	virtual ~solver();
+	static string exec(const char* cmd);
+	static void create_gmpl_with_data(vector<vector<float>> Alpha, float Beta,
+			vector<vector<float>> Gamma, vector<float> Min_demand,
+			vector<float> Max_demand);
+	solver solve_problem();
+};
+
+#endif /* SOLVER_H_ */
