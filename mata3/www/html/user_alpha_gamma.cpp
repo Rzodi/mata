1307083e9d1a3e60b85aedd442b09a11b865e6d0
kkoralewicz <krykor95@gmail.com>
2019-01-02 12:16:36 +0100

    modify alpha/gamma function

--- mata3/www/html/user_alpha_gamma.cpp
+++ mata3/www/html/user_alpha_gamma.cpp
@@ -26,9 +26,9 @@ vector<vector<float>> user_alpha_gamma::user_position_demands(int users, float m
 	return my_vector;
 }
 
-vector<vector<float>> user_alpha_gamma::f_alpha(int U_count, int ENB_count, vector<Position> enb_positions, vector<float> x, vector<float> y, float R, float worst_alpha) // definicja funkcji dwuwymiarowej na vectorze
+vector<vector<float>> user_alpha_gamma::f_alpha(int U_count, int ENB_count, vector<Position> enb_positions, vector<float> x, vector<float> y, float R, float worst_alpha, float best_alpha) // definicja funkcji dwuwymiarowej na vectorze
 {
-	float best_alpha = 0.001; // najlepsza alpha, uzywana do wyznaczenia rownania funkcji liniowej - patrz komentarz po main()
+	//float best_alpha = 0.001; // najlepsza alpha, uzywana do wyznaczenia rownania funkcji liniowej - patrz komentarz po main()
 	//int worst_alpha = 25; // najgorsza alpha, uzywana do wyznaczenia rownania funkcji liniowej - patrz komentarz po main()
 
 	vector< vector<float> > dist_enb(U_count, vector<float>(ENB_count)); // definiujemy wektor
@@ -40,14 +40,18 @@ vector<vector<float>> user_alpha_gamma::f_alpha(int U_count, int ENB_count, vect
 		{
 			dist_enb[u][e] = sqrt(pow(enb_positions[e].X - x[u], 2) + pow(enb_positions[e].Y - y[u], 2)); // liczenie odleglosci usera od ENB w ukladzie kartezjanskim
 			alpha[u][e] = (((worst_alpha - best_alpha) / R) * dist_enb[u][e]) + best_alpha; // liczenie alpha'y z funkcji liniowej w zaleznosci od odleglosci od ENB
+			if (alpha[u][e] > worst_alpha) // czyli 25
+			{
+				alpha[u][e] = 26;
+			}
 		}
 	}
 	return alpha;
 }
 
-vector<vector<float>> user_alpha_gamma::f_gamma(int U_count, int wifi_count, vector<Position> router_positions, vector<float> x, vector<float> y, float r, float worst_gamma) // definicja funkcji dwuwymiarowej na vector'ze
+vector<vector<float>> user_alpha_gamma::f_gamma(int U_count, int wifi_count, vector<Position> router_positions, vector<float> x, vector<float> y, float r, float worst_gamma, float best_gamma) // definicja funkcji dwuwymiarowej na vector'ze
 {
-	float best_gamma = 0.001; // najlepsza gamma, uzywana do wyznaczenia rownania funkcji liniowej - patrz komentarz po main()
+	//float best_gamma = 0.001; // najlepsza gamma, uzywana do wyznaczenia rownania funkcji liniowej - patrz komentarz po main()
 	//int worst_gamma = 1; // najgorsza gamma, uzywana do wyznaczenia rownania funkcji liniowej - patrz komentarz po main()
 
 	vector< vector<float> > dist_wifi(U_count, vector<float>(wifi_count)); // definiujemy wektor
@@ -59,6 +63,10 @@ vector<vector<float>> user_alpha_gamma::f_gamma(int U_count, int wifi_count, vec
 		{
 			dist_wifi[u][w] = sqrt(pow(router_positions[w].X - x[u], 2) + pow(router_positions[w].Y - y[u], 2)); // liczenie odleglosci usera od routera WIFI w ukladzie kartezjanskim
 			gamma[u][w] = (((worst_gamma - best_gamma) / r) * dist_wifi[u][w]) + best_gamma; // liczenie gamma'y z funkcji liniowej w zaleznosci od odleglosci od routera wifi
+			if (gamma[u][w] > worst_gamma) // czyli 1
+			{
+				gamma[u][w] = 1;
+			}
 		}
 	}
 	return gamma;
