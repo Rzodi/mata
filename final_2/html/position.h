5c493ef770f1011ee30ab3c208633e95732716ff
Lukasz Rzodeczko <rzodeczkol@gmail.com>
2019-01-03 11:50:32 +0100

    working version

---
+++ final_2/html/position.h
@@ -0,0 +1,18 @@
+#ifndef POSITION_H_
+#define POSITION_H_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Position{
+    public:
+    float X;
+    float Y; 
+    Position(float x_position, float y_position);    
+};
+
+
+#endif /* POSITION_H_ */
